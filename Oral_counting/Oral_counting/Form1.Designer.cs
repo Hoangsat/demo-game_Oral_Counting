a08faccd6a20f6a3af46395696b287feeb4b392d
Hoangsat <hoangsat1@gmail.com>
2022-04-17 21:52:30 +0200

    oc

---
+++ Oral_counting/Oral_counting/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace Oral_counting
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label_think = new System.Windows.Forms.Label();
+            this.text_answer = new System.Windows.Forms.TextBox();
+            this.button_plus = new System.Windows.Forms.Button();
+            this.progress = new System.Windows.Forms.ProgressBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label_think
+            // 
+            this.label_think.BackColor = System.Drawing.Color.White;
+            this.label_think.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label_think.Font = new System.Drawing.Font("Comic Sans MS", 48F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_think.Location = new System.Drawing.Point(48, 30);
+            this.label_think.Name = "label_think";
+            this.label_think.Size = new System.Drawing.Size(481, 97);
+            this.label_think.TabIndex = 0;
+            this.label_think.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // text_answer
+            // 
+            this.text_answer.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.text_answer.Font = new System.Drawing.Font("Comic Sans MS", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.text_answer.Location = new System.Drawing.Point(622, 30);
+            this.text_answer.Name = "text_answer";
+            this.text_answer.Size = new System.Drawing.Size(175, 97);
+            this.text_answer.TabIndex = 1;
+            this.text_answer.KeyDown += new System.Windows.Forms.KeyEventHandler(this.text_answer_KeyDown);
+            // 
+            // button_plus
+            // 
+            this.button_plus.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button_plus.Location = new System.Drawing.Point(48, 150);
+            this.button_plus.Name = "button_plus";
+            this.button_plus.Size = new System.Drawing.Size(104, 38);
+            this.button_plus.TabIndex = 2;
+            this.button_plus.Text = "Plus";
+            this.button_plus.UseVisualStyleBackColor = true;
+            this.button_plus.Click += new System.EventHandler(this.button_plus_Click);
+            // 
+            // progress
+            // 
+            this.progress.Location = new System.Drawing.Point(622, 148);
+            this.progress.Name = "progress";
+            this.progress.Size = new System.Drawing.Size(175, 40);
+            this.progress.TabIndex = 3;
+            this.progress.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(172, 150);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 38);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Plus";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(300, 150);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(104, 38);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Plus";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(425, 150);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(104, 38);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Plus";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(821, 242);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.progress);
+            this.Controls.Add(this.button_plus);
+            this.Controls.Add(this.text_answer);
+            this.Controls.Add(this.label_think);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Oral counting";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label_think;
+        private System.Windows.Forms.TextBox text_answer;
+        private System.Windows.Forms.Button button_plus;
+        private System.Windows.Forms.ProgressBar progress;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Oral_counting/Oral_counting/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Oral_counting
+{
+    public partial class Form1 : Form
+    {
+        int count;
+        int min = 1;
+        int max = 10;
+        int count_max = 3;
+        Random rnd = new Random();
+        string answer;
+        public Form1()
+        {
+            
+            InitializeComponent();
+            start();
+        }
+
+        private void button_plus_Click(object sender, EventArgs e)
+        {
+            start();
+        }
+        private void start()
+        {
+            progress.Visible = true;
+            progress.Minimum = 0;
+            progress.Maximum = count_max;
+            progress.Value = 0;
+            button_plus.Enabled = false;
+            count = 0;
+            think();
+        }
+        private void think()
+        {
+            
+            int a, b;
+            a = rnd.Next(min, max + 1);
+            b = rnd.Next(min, max + 1);
+            label_think.Text = a.ToString() + " + " + b.ToString() + " = ";
+            answer = (a + b).ToString();
+            count++;
+        }
+
+        private void text_answer_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                if (text_answer.Text == answer)
+                {
+                    
+                    progress.Value = count;
+                    if (count == count_max)
+                    {
+                        gamewin();
+                        return;
+                    }
+                    else
+                    {
+                        text_answer.Text = "";
+                        think();
+                    }
+                }
+                else
+                {
+                    gameover();
+                }
+            }
+        }
+        private void gameover()
+        {
+            text_answer.Text = answer;
+            done();
+        }
+        private void gamewin()
+        {
+            MessageBox.Show("WIN!!");
+            done();
+        }
+        private void done()
+        {
+            button_plus.Enabled = true;
+            progress.Visible = false;
+            label_think.Enabled = false;
+            text_answer.Enabled = false;
+            button_plus.Focus();
+        }
+    }
+}
